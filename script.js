eb5f11663d0bd00d4c9fc143be5d5b76609ad669
YusufOkmen <yusufkmen@hotmail.com>
2025-10-18 15:25:04 +0300

    Worked on how to apply input data into todosContainer

--- script.js
+++ script.js
@@ -1,6 +1,37 @@
-document.addEventListener("DOMContentLoaded", () => 
-{
+document.addEventListener("DOMContentLoaded", () => {
     const taskInput = document.getElementById("taskInput");
-    
+    const addTaskBtn = document.getElementById("addTaskBtn");
+    const taskList = document.getElementById("taskList");
+    const emptyImage = document.querySelector(".emptyImg");
+
+    const toggleEmptyState = () => {
+        emptyImage.style.display = taskList.children.length === 0 ? "block" : "none";
+    }
+
+    const addTask = (event) => {
+        event.preventDefault();
+        const taskText = taskInput.value.trim();
+        if (!taskText) {
+            return;
+        }
+
+        const li = document.createElement("li");
+        li.innerHTML = `
+            <input type="checkbox" class="checkbox">
+            <span>${taskText}</span>
+        `;
+
+        taskList.appendChild(li)
+        taskInput.value = " ";
+        toggleEmptyState();
+    };
+
+    addTaskBtn.addEventListener("click", addTask);
+    taskInput.addEventListener("keypress", (e) => {
+        if (e.key === "Enter") {
+            addTask(e);
+        }
+    })
+
 
 })
