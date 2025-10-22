9e2dfdf5f360610bc32984a7d7a2cece8116bd44
YusufOkmen <yusufkmen@hotmail.com>
2025-10-22 11:03:45 +0300

    Added edit and delete buttons to taksk

--- script.js
+++ script.js
@@ -3,9 +3,13 @@ document.addEventListener("DOMContentLoaded", () => {
     const addTaskBtn = document.getElementById("addTaskBtn");
     const taskList = document.getElementById("taskList");
     const emptyImage = document.querySelector(".emptyImg");
+    const todosContainer = document.querySelector(".todosContainer");
 
     const toggleEmptyState = () => {
+        // Makes empty image display and disappear
         emptyImage.style.display = taskList.children.length === 0 ? "block" : "none";
+        // Controls the width of the todosContainer based on the listed tasks
+        todosContainer.style.width = taskList.children.length > 0 ? "100%" : "50%";
     }
 
     const addTask = (event) => {
@@ -19,7 +23,49 @@ document.addEventListener("DOMContentLoaded", () => {
         li.innerHTML = `
             <input type="checkbox" class="checkbox">
             <span>${taskText}</span>
+            <div class="taskButtons">
+                <button class="editBtn">
+                    <i class="fa-solid fa-pen"></i>
+                </button>
+                <button class="deleteBtn">
+                    <i class="fa-solid fa-trash"></i>
+                </button>
+                
+            </div>
         `;
+
+        const checkbox = li.querySelector(".checkbox");
+        const editBtn = li.querySelector(".editBtn");
+
+        if (completed) {
+            li.classList.add("completed");
+            editBtn.disabled = true;
+            editBtn.style.opacity = "0.5";
+            editBtn.style.pointerEvents = "none";
+        }
+
+        checkbox.addEventListener("change", () => {
+            const isChecked = checkbox.checked;
+            li.classList.toggle("completed", isChecked)
+            editBtn.disabled = isChecked;
+            editBtn.style.opacity = isChecked ? "0.5" : "1";
+            editBtn.style.pointerEvents = isChecked ? "none" : "auto";
+        })
+
+        editBtn.addEventListener("click", () => {
+            if(!checkbox.checked) {
+                taskInput.value = li.querySelector
+                ("span").textContent;
+                li.remove();
+            }
+        })
+
+        const deleteBtn = li.querySelector(".deleteBtn");
+
+        deleteBtn.addEventListener("click", () => {
+            li.remove();
+            toggleEmptyState();
+        })
 
         taskList.appendChild(li)
         taskInput.value = " ";
